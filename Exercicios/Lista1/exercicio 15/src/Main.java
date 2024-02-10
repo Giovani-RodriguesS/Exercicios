e23fd15d9c19088e31998cb17856d7efd00071c7
Giovani-RodriguesS <giovanirodrigues021@gmail.com>
2024-02-09 22:07:57 -0300

    secund commit

--- Exercicios/Lista1/exercicio 15/src/Main.java
+++ Exercicios/Lista1/exercicio 15/src/Main.java
@@ -1,6 +1,18 @@
+import java.util.Scanner;
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println(); 
+        double volume,altura,raio;
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Digite o valor da altura e do raio, respectivamente, de um " +
+                            "cilindro, para calcular seu volume: (considere pi (3.14) e medidas em CM ");
+        altura = in.nextDouble();
+        System.out.println("Agora o raio do cilindro");
+        raio = in.nextDouble();
+        volume = 3.14 * Math.pow(raio, 2) * altura;
+        System.out.println("O volume desse cilindro é: " + volume);
+
     }
 }
-//Receber o raio e a altura de um cilindro e calcular seu volume.
