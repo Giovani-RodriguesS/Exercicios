a74a20fd548bbfc40c24827667294ed9b4c0310f
Giovani-RodriguesS <giovanirodrigues021@gmail.com>
2024-02-21 21:42:29 -0300

    semaforo

--- Exercicios/Lista 2/exercicio 14/src/Main.java
+++ Exercicios/Lista 2/exercicio 14/src/Main.java
@@ -1,5 +1,8 @@
+import java.util.*;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Scanner in = new Scanner(System.in);
+        System.out.println(" ");
     }
 }
