8002370e5d02e3d5e68112d9387dd3ae5fb75a46
Giovani-RodriguesS <giovanirodrigues021@gmail.com>
2024-02-29 23:00:07 -0300

    29-02

--- Exercicios/L.P/Lista 3/exercicio 1/src/Main.java
+++ Exercicios/L.P/Lista 3/exercicio 1/src/Main.java
@@ -1,8 +1,16 @@
 import java.util.*;
 public class Main {
     public static void main(String[] args) {
+
         int num1,i;
         Scanner in = new Scanner(System.in);
+
         System.out.println("Digite um numero e sua tabuada será impressa");
+        num1 = in.nextInt();
+        i = 0;
+        while (i<11) {
+            System.out.println(num1 + " X " + i + " = " + (num1*i));
+            i++;
+        }
     }
 }
--- Exercicios/L.P/Lista 3/exercicio 2/src/Main.java
+++ Exercicios/L.P/Lista 3/exercicio 2/src/Main.java
@@ -2,7 +2,18 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
 
+        int num1,num2,num3,num4,num5;
         Scanner in = new Scanner(System.in);
-        System.out.println("");
+//  Entradas
+        System.out.println("Digite cinco números inteiros seguidamente: ");
+        num1 = in.nextInt();
+        num2 = in.nextInt();
+        num3 = in.nextInt();
+        num4 = in.nextInt();
+        num5 = in.nextInt();
+//  Processamento
+        
+
+
     }
 }
