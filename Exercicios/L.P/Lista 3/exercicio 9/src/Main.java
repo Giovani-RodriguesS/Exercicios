557b508ee48972e507fcfb60c0c73f46aa155f27
g_gab <giovanirodrigues021@gmail.com>
2024-03-05 21:30:52 -0300

    feat: ex 9

--- Exercicios/L.P/Lista 3/exercicio 9/src/Main.java
+++ Exercicios/L.P/Lista 3/exercicio 9/src/Main.java
@@ -1,43 +1,53 @@
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-//  Variaveis
 
-        int resposta,continuarJogo;
+        //  Variaveis
+
+        int resposta;
         boolean continuar = true;
+        boolean continuarWhile = true;
         Scanner in = new Scanner(System.in);
 
 //Início do prog
         System.out.println("-----Jogo da Tabuada-----\n");
         System.out.println("Insira o valor da operação a seguir corretamente para continuar o jogo: ");
 //início do looping
-
-        while (continuar){
-
-            for(int i=1;i<=10;i++){
-                if(continuar == false)
-                    break;
-                for(int j=1;j<=10;j++) {
+        while (continuarWhile){
+            for (int i=1;i<=1 && continuar;i++){
+                for (int j=1;j<=10;j++){
                     System.out.println(i+" X "+j+" = ?");
                     resposta = in.nextInt();
-                    if (resposta != i*j){
+                    if(resposta != i*j){
                         continuar = false;
                         break;
                     }
                 }
-//continuar prog
-
-            System.out.println("Quer continuar? Sim(1),Não(0)");
-            continuarJogo = in.nextInt();
-
-            if(continuarJogo == 1)
-                continuar = true;
-
-            else if(continuarJogo == 0) {
-                System.out.println("Fim de Jogo");
-                continuar = false;
-                }
             }
+//resposta errada
+            if (continuar == false){
+                System.out.println("Você errou! Quer continuar? Sim(1), Não(0)");
+                resposta = in.nextInt();
+
+                if(resposta == 1)
+                    continuar = true;
+                else {
+                    System.out.println("Fim de Jogo");
+                    continuarWhile = false;
+                    }
+//acertou tudo
+            }else {
+                System.out.println("Parabéns, Você acertou tudo! Quer jogar novamente? Sim(1), Não(0)");
+                resposta = in.nextInt();
+
+                if(resposta == 1)
+                    continuarWhile = true;
+                else {
+                    System.out.println("Fim de Jogo");
+                    continuarWhile = false;
+                    }
+            }
+        }
+
     }
-}
 }
