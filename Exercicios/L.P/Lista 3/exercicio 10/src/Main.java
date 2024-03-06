e75def5bc96dd912475027a53dfea4428d9ed529
Giovani-RodriguesS <giovanirodrigues021@gmail.com>
2024-03-06 12:43:00 -0300

    feat:ex 10

--- Exercicios/L.P/Lista 3/exercicio 10/src/Main.java
+++ Exercicios/L.P/Lista 3/exercicio 10/src/Main.java
@@ -1,27 +1,24 @@
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        int n,num1,num2,aux,resultado;
+        int n,proximo,anterior,aux,resultado;
         Scanner in = new Scanner(System.in);
 
         System.out.println("Sequência Fibonacci\n");
         System.out.println("Insira até qual termo deseja imprimir");
         n = in.nextInt();
-        num1 = 0;
-        num2 = 1;
+        proximo = 0;
+        anterior = 0;
         aux = 0;
         for (int i = 0; i < n;i++){
-            if (i<2) {
-                System.out.println(aux);
-                aux++
+            if(i==1) {
+                proximo = 1;
+                System.out.println(proximo);
             }
-            else {
-                resultado = num1 + num2;
-                aux = num2;
-                num1 = aux;
-                num2 = resultado;
-                System.out.println(resultado);
-            }
+            resultado = proximo + anterior;
+            anterior = proximo;
+            proximo = resultado;
+            System.out.println(resultado);
         }
     }
 }
