4558b2c9d1b430d6256f56cc46c3b7bb1b7fff5e
Giovani-RodriguesS <giovanirodrigues021@gmail.com>
2024-04-02 22:50:57 -0300

    feat: script.js
    new feature added in script.js

--- Web/Calculadora/assets/JS/script.js
+++ Web/Calculadora/assets/JS/script.js
@@ -1,41 +1,97 @@
+//  Var
 const result = document.querySelector('.result');
 const botton = document.querySelector('.buttons button');
-const resultIn = result.innerText;
-result.value = ''
-let aux = 0;
+
+//  stack
 let numbers_stack = []
 let operator_stack =[]
 let number_complete = false
+let count = 1;
+result.value = ''
 
-if(number_complete)
-    clearDisplay();
 
+//  Functions
 function clearDisplay () {
     result.innerText = '0'
     result.value = '';
+    count = 1
+    number_complete = false
+
 }
+
 function bottonNum (num) {
-    result.innerText =  result.value += num;
+    if(number_complete)
+        clearDisplay();
+    if(count < 10)
+    if (!(result.value == '0' && num == '0') && (num != ',' || result.value.indexOf(",") < 0))   
+        if(result.value == '0' && num != ",")
+            result.innerText =  result.value = num;
+        else
+            result.innerText =  result.value += num;
+        count++
 }
-function operator (operator) {
-    if (operator == "+")
-        result.value   
-}
-//result.value = eval(value)
-//if(value != '.' || result.value.indexOf(".") < 0)
-//    value = result.value + value;
 
-function push(value) {
-
+function calculate () {
+    addNumberToStack();
+    let result=0;
+    
+    while (operator_stack.length()>0) {
+        result = partCalc();
+        addResultToStack(result);
+    } 
+    alert(result);
+    result.innerText = result.valueOf = resultOfCalc;
 }
-function pop() {
 
-}
 function partCalc () {
     let n2 = numbers_stack.pop();
     let n1 = numbers_stack.pop();
     let op = operator_stack.pop();
-    let resultShow = eval()
+    let resultOfCalc = eval(n1 + op + n2)
+    return resultOfCalc;
+       
+}
 
+function addNumberToStack(){
+    number_complete = true;
+    numbers_stack.push(result.value);
+}
 
+function operator (value) {
+    if(!number_complete) {
+        addNumberToStack()
+        while (operator_stack.length() > 0 && !precedence(topOfOperartorStack(), value)){
+            let resultOfCalc = partCalc();
+            addResultToStack(resultOfCalc);
+        }
+    } 
+    else {
+        operator_stack.pop()
+    }
+    addOperatorToStack(value)
+    alert(operator_stack)
+}
+
+function addOperatorToStack(value) {
+    operator_stack.push(value);
+}
+
+function precedence() {
+    let operators = new Map([
+        ['+',1],
+        ['-',1],
+        ['/',2],
+        ['*',2],
+        ['(',3],
+        [')',3]
+    ])
+    return operators.get(op2) > operators.get(op1) 
+}
+function topOfOperartorStack() {
+    return operator_stack[operator_stack.length-1]
+}
+
+//adiciona na pilha o numero
+function addResultToStack(number) {
+    numbers_stack.push(number)
 }
