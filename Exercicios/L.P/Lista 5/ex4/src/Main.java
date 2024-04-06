a57c8da021d4693e03d24dbd4c93530a44852d60
Giovani-RodriguesS <giovanirodrigues021@gmail.com>
2024-04-05 21:55:01 -0300

    feat: ex4
    ex4 was refactored (recursion)

--- Exercicios/L.P/Lista 5/ex4/src/Main.java
+++ Exercicios/L.P/Lista 5/ex4/src/Main.java
@@ -2,13 +2,16 @@ import java.util.Scanner;
 
 public class Main {
 
-    static boolean palindromo (String p){
-        for (int j=0;j<p.length()-1;j++){
+    static boolean palindromo (String p, int n){
+        for (int j=n;j<(p.length()-1)/2;j++){
             if(Character.isLetterOrDigit(p.charAt(j))) // Verifica se é um digito/letra
                 if (Character.toLowerCase(p.charAt(j)) != Character.toLowerCase(p.charAt(p.length()-j-1))) // verifica se os chars são diferentes
                     return false;
         }
-        return true;
+        if(n!= (p.length()-1)/2)
+            return palindromo(p,n+1);
+        else
+            return true;
     }
 
     public static void main(String[] args) {
@@ -18,7 +21,7 @@ public class Main {
 
         System.out.println("Digite uma palavra: ");
         palavra = in.next();
-        if (palindromo(palavra))
+        if (palindromo(palavra, 0))
             System.out.println("É um palíndromo");
         else
             System.out.println("Não é um palíndromo");
