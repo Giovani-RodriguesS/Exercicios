a3c4a2f367fb72a78eadeff4d3eefd2ff41016eb
Giovani-RodriguesS <giovanirodrigues021@gmail.com>
2024-04-11 22:55:52 -0300

    objeto

---
+++ Exercicios/L.P/Lista 5/Objeto/src/Carro.java
@@ -0,0 +1,47 @@
+public class Carro {
+    //atributos
+    private String cor;
+    private int posX;
+    private int posY;
+    private int largura;
+    private int altura;
+    //construtor
+    public void Carro(String cor,int posX, int posY,int largura, int altura){
+       this.cor = cor;
+       this.posX = posX;
+       this.posY = posY;
+       this.largura = largura;
+       this.altura = altura;
+   }
+
+
+    //metodos
+    public void andarFrente(){
+        if(posY > 0)
+            posY--;
+    }
+    public void andarRe(){
+        if(posY < 1080-200)
+            posY++;
+    }
+    public void andarDireita(){
+        if(posX < 1920-80)
+            posX++;
+    }
+    public void andarEsquerda(){
+        if(posX > 0)
+            posX--;
+    }
+    //To string
+
+    @Override
+    public String toString() {
+        return "Carro{" +
+                "cor='" + cor + '\'' +
+                ", posX=" + posX +
+                ", posY=" + posY +
+                ", largura=" + largura +
+                ", altura=" + altura +
+                '}';
+    }
+}
