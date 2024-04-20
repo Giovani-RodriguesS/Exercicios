91226d76524e7e70013023e1019aae8750df5f28
Giovani-RodriguesS <giovanirodrigues021@gmail.com>
2024-04-19 22:15:24 -0300

    feat: testes file

--- Exercicios/L.P/Lista 5/testes/src/Main.java
+++ Exercicios/L.P/Lista 5/testes/src/Main.java
@@ -5,12 +5,14 @@ public class Main {
         Scanner in = new Scanner(System.in);
         int numDec;
         int [] numBin = new int[8];
-
+        //  entrada
         System.out.println("Digite um numero decimal inteiro para encontrar seu binário");
         numDec = in.nextInt();
+        //  transformação p binario
         binario(numDec,0 ,numBin);
+        //  saida
         for(int i=0;i<8;i++){
-            if(i%4 == 0)
+            if(i%4 == 0 && i != 0)
                 System.out.print(" "+numBin[8-1-i]);
             else
                 System.out.print(numBin[8-1-i]);
@@ -20,19 +22,22 @@ public class Main {
         int aux;
         int rest = numDec%2; //resto
 
-        if(numDec == 1) {
+        if(numDec == 1) { // retorna a função (1)
             numBin[i] = 1;
             return;
         }
-        else if (numDec == 0) {
+        else if (numDec == 0) { // retorna a função (0)
             numBin[i] = 0;
             return;
         }
+        // divisões de numDec
         if(rest == 1)
             aux = (numDec - 1) / 2;
         else
             aux = numDec/2;
+        // salva o resto no vetor
         numBin[i] = rest;
+        // Recursividade
         if(numDec > 1)
             binario(aux, i+1, numBin);
     }
