2fcdf3f99dde085d1c90d7f169bcf53537e840ff
Giovani-RodriguesS <giovanirodrigues021@gmail.com>
2024-10-24 21:00:52 -0300

    feat: asp net mvc was created

--- Exercicios/2 Sem/dotnet/Cafeteria/obj/Debug/net8.0/Cafeteria.AssemblyInfo.cs
+++ Exercicios/2 Sem/dotnet/Cafeteria/obj/Debug/net8.0/Cafeteria.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Cafeteria")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f98ce9c98b9961ca91323f7a011c31a8c29a6d93")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d1ebf4b36eaee431cd043a94376253a0e7aba2b9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Cafeteria")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Cafeteria")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
